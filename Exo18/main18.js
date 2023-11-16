8b207132d2c628837a7a104cb080b094e42e0385
FracoisMRib <fr.ribere@gmail.com>
2023-11-16 17:02:23 +0100

    sauvegarde pour eval

---
+++ Exo18/main18.js
@@ -0,0 +1,50 @@
+/*
+Exercice 18 Classe et Objet :
+Gérer une PME
+Cahier des charges :
+
+-Un Salarié a un nom, prénom, âge, salaire mensuel
+Il est payé sur N mois.
+En plus il y a XXX de charges
+
+-Une Pme c’est un nom, une équipe de plusieurs salariés
+Grace à ses ventes elle a des revenus R
+Mais aussi … : 
+des frais fixes FF (impôts etc…)
+Des frais d’achats de matériel et de logiciels FA
+
+TODO : 
+Créer une classe Pme et une classe Employee
+Utiliser des fonctions
+Faire le bilan annuel de l’entreprise et l’afficher en console.
+(Bilan reste en solde de la Pme)
+
+Détails : 
+3 salariés qui gagnent par mois : 2000, 3000 et 4000 euros
+R = 300000 (trois cent mille)
+FF = 20000 (vingt mille)
+FA = 50000 (cinquante mille)
+N = 12
+XXX = 10%
+*/
+
+class Pme {
+    constructor(nom, nbrSalaries, revenus, fraisFixes, fraisAchats) {
+        this.nom = nom;
+        this.revenus = revenus;
+        this.nbrSalaries = nbrSalaries;
+        this.fraisAchats = fraisAchats;
+        this.fraisFixes = fraisFixes;
+    }
+}
+
+class Employee {
+    constructor(nom, prenom, age, salaireMens, nbrMois, nbrCharges) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+        this.salaireMens = salaireMens;
+        this.nbrMois = nbrMois;
+        this.nbrCharges = nbrCharges;
+    }
+}
---
+++ ExoEval/main (1).js
@@ -0,0 +1,103 @@
+const usersHuman = [{
+        type: "humain",
+        name: "John Doe",
+        email: "j.smith@gmail.com",
+        age: 25
+    },
+    {
+        type: "humain",
+        name: "Jane Smith",
+        email: "ja.doe@sfr.fr",
+        age: 5
+    },
+    {
+        type: "humain",
+        name: "Le Vénérable",
+        email: "levy@gmail.com",
+        age: 500
+    }
+];
+
+const usersPet = [{
+        type: "animal de compagnie",
+        espece: "chien",
+        name: "Rox",
+        age: 7,
+        propriétaire: "John Doe"
+    },
+    {
+        type: "animal de compagnie",
+        espece: "renard",
+        name: "Roukie",
+        age: 300,
+        propriétaire: "Le Vénérable"
+    }
+];
+
+const usersXeno = [{
+        type: "Xeno",
+        espece: "Krogan",
+        name: "Wrex",
+        menace: "Rouge",
+        age: 45
+    },
+    {
+        type: "Xeno",
+        espece: "Turien",
+        name: "Garrus",
+        menace: "Vert",
+        age: 35
+    },
+    {
+        type: "Xeno",
+        espece: "Asari",
+        name: "Liara",
+        menace: "ULTRA Rouge",
+        age: 25
+    }
+];
+
+//console.log(usersXeno[0]);
+let tabData = [];
+
+tabData.push(usersHuman);
+tabData.push(usersPet);
+tabData.push(usersXeno);
+
+console.log(tabData);
+
+function afficherHumain(objet) {
+    console.log(objet[1]);
+    console.log(objet.email);
+    console.log(objet.age);
+} 
+
+function afficherAnimal(objet) {
+    console.log(objet.name);
+    console.log(objet.espace);
+    console.log(objet.age);
+    console.log(objet.propriétaire);
+} 
+
+function afficherXeno(objet) {
+    console.log(objet.name);
+    console.log(objet.espece);
+    console.log(objet.age);
+    console.log(objet.menace);
+} 
+
+function profil(objet) {
+    if(objet == usersHuman) {
+        return afficherHumain;
+    } else if (objet == usersPet) {
+        console.log(afficherAnimal);
+    } else if (objet == usersXeno) {
+        console.log(afficherXeno);
+    } else {
+        console.log("Type de profil non existant")
+    }
+}
+
+profil(usersHuman);
+profil(usersPet);
+profil(usersXeno);
